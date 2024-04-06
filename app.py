eac435111a7e014525c2c08e1e3942063f5037bb
Aum Pauskar <aumpauskar111@gmail.com>
2024-04-06 18:16:08 +0530

    route returning csv file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 import fastf1
+import os
 
 app = Flask(__name__)
 
@@ -11,18 +12,22 @@ def index():
 def get_session_data(year, event, session, driver):
     try:
         print(year, event, session, driver)
-        event = event + "Grand Prix"
+        event = event + " Grand Prix"
         data = fastf1.get_session(year, event, session)
         data.load()
         driver_data = data.laps.pick_driver(driver)
         telem = driver_data.telemetry
         print(telem)
-        # returning 200 OK response
-        return telem.to_json(), 200
+
+        # Save telemetry data to a CSV file
+        csv_file = f'{driver}_telemetry_{year}_{event}_{session}.csv'
+        telem.to_csv(csv_file, index=False)
+
+        # Return the CSV file for download
+        return send_file(csv_file, as_attachment=True, download_name=csv_file)
     except Exception as e:
         # returning 400 Bad Request response
         return str(e), 400
-    
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
