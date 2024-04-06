6b960d65491678e8f4fdeb1386c399fb79b66817
Aum Pauskar <aumpauskar111@gmail.com>
2024-04-06 18:21:25 +0530

    copy of the file is removed from the server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, after_this_request
 import fastf1
 import os
+import tempfile
 
 app = Flask(__name__)
 
@@ -19,12 +20,21 @@ def get_session_data(year, event, session, driver):
         telem = driver_data.telemetry
         print(telem)
 
-        # Save telemetry data to a CSV file
-        csv_file = f'{driver}_telemetry_{year}_{event}_{session}.csv'
-        telem.to_csv(csv_file, index=False)
+        # Save telemetry data to a temporary CSV file
+        with tempfile.NamedTemporaryFile(suffix=".csv", delete=False) as temp:
+            csv_file = temp.name
+            telem.to_csv(csv_file, index=False)
 
-        # Return the CSV file for download
-        return send_file(csv_file, as_attachment=True, download_name=csv_file)
+            # Return the CSV file for download
+            @after_this_request
+            def remove_file(response):
+                try:
+                    os.remove(csv_file)
+                except Exception as error:
+                    app.logger.error("Error removing or closing temporary file %s: %s", csv_file, error)
+                return response
+
+            return send_file(csv_file, as_attachment=True, download_name=f'{driver}_telemetry_{year}_{event}_{session}.csv')
     except Exception as e:
         # returning 400 Bad Request response
         return str(e), 400
